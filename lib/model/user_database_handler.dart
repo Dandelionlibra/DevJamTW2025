1831a7b5a41de78f1a00b5ad066cad0bd5e5388e
ja-errorpro <compileerr0r@ja-errorpro.codes>
2025-06-07 19:21:02 +0800

    refactor: update UserModel and implement UserDatabaseHandler

---
+++ lib/model/user_database_handler.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+import 'dart:convert';
+import 'user_model.dart';
+
+class UserDatabaseHandler {
+  static final String getDatabaseUrl =
+      'https://n8n.ja-errorpro.codes/webhook/b9ecbf82-b060-4d25-9747-bcf55e1bc063';
+  static final String updateDatabaseUrl =
+      'https://n8n.ja-errorpro.codes/webhook/d4239cdb-62b9-44d8-86fc-15dd90915b1a/';
+  static final String registerDatabaseUrl =
+      'https://n8n.ja-errorpro.codes/webhook/7961c17f-8830-46ad-bf6c-47499dd550cf';
+
+  static Future<void> saveUserProfile(UserModel user) async {
+    final url = Uri.parse(updateDatabaseUrl);
+    final response = await HttpClient().postUrl(url);
+    response.headers.set('Content-Type', 'application/json');
+    response.add(utf8.encode(json.encode(user.toMap())));
+    final HttpClientResponse httpResponse = await response.close();
+    if (httpResponse.statusCode != 200) {
+      throw Exception(
+        'Failed to save user profile: ${httpResponse.statusCode}',
+      );
+    }
+  }
+
+  static Future<UserModel?> getUserProfile(String uid) async {
+    final url = Uri.parse(getDatabaseUrl);
+    final response = await HttpClient().getUrl(
+      Uri.https(getDatabaseUrl, '', {'uid': uid}),
+    );
+    final HttpClientResponse httpResponse = await response.close();
+    if (httpResponse.statusCode == 200) {
+      final String jsonString = await httpResponse
+          .transform(utf8.decoder)
+          .join();
+      return UserModel.fromMap(json.decode(jsonString));
+    } else if (httpResponse.statusCode == 404) {
+      return null; // User not found
+    } else {
+      throw Exception(
+        'Failed to fetch user profile: ${httpResponse.statusCode}',
+      );
+    }
+  }
+}
--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -1,26 +1,28 @@
-import 'package:Wandering/preference_data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'user_database_handler.dart';
-import 'package:Wandering/auth.dart';
-import 'place_model.dart';
+import 'package:devjam_tw2025/auth.dart';
 
 class UserModel {
   final String uid;
   final User firebaseUser;
   final String username;
   final String email;
-  UserPreferences? preferences;
-  List<PlaceModel> favoritePlaces = [];
+  String? gender;
+  String? age;
+  String? height;
+  String? weight;
 
   UserModel({
     required this.uid,
     required this.username,
     required this.firebaseUser,
     required this.email,
-    this.preferences,
-    this.favoritePlaces = const [],
+    this.gender,
+    this.age,
+    this.height,
+    this.weight,
   });
 
   // Convert UserModel to Map
@@ -29,8 +31,10 @@ class UserModel {
       'uid': uid,
       'username': username,
       'email': email,
-      'preferences': preferences?.toMap(),
-      'favoritePlaces': favoritePlaces.map((place) => place.toMap()).toList(),
+      'gender': gender,
+      'age': age,
+      'height': height,
+      'weight': weight,
     };
   }
 
@@ -41,73 +45,10 @@ class UserModel {
       username: map['username'],
       firebaseUser: map['firebaseUser'],
       email: map['email'],
-      preferences: UserPreferences.fromMap(map['preferences']),
-      favoritePlaces: (map['favoritePlaces'] as List)
-          .map((place) => PlaceModel.fromMap(place))
-          .toList(),
-    );
-  }
-
-  void addFavoritePlace(PlaceModel place) {
-    favoritePlaces.add(place);
-  }
-
-  bool removeFavoritePlace(PlaceModel place) {
-    return favoritePlaces.remove(place);
-  }
-
-  Future<void> updatePreferences(
-    BuildContext context,
-    UserPreferences newPreferences,
-  ) async {
-    preferences = newPreferences;
-    await UserDatabaseHandler.instance.insertOrUpdateUser(
-      this,
-      Provider.of<AuthModel>(context, listen: false),
-    );
-  }
-
-  Future<void> updateTravelStyle(
-    BuildContext context,
-    List<String> travelStyle,
-  ) async {
-    preferences?.travelStyles = Set<String>.from(travelStyle);
-    await UserDatabaseHandler.instance.insertOrUpdateUser(
-      this,
-      Provider.of<AuthModel>(context, listen: false),
-    );
-  }
-
-  Future<void> updateLocationType(
-    BuildContext context,
-    List<String> locationType,
-  ) async {
-    preferences?.locationTypes = Set<String>.from(locationType);
-    await UserDatabaseHandler.instance.insertOrUpdateUser(
-      this,
-      Provider.of<AuthModel>(context, listen: false),
-    );
-  }
-
-  Future<void> updateAvoidType(
-    BuildContext context,
-    List<String> avoidType,
-  ) async {
-    preferences?.avoidTypes = Set<String>.from(avoidType);
-    await UserDatabaseHandler.instance.insertOrUpdateUser(
-      this,
-      Provider.of<AuthModel>(context, listen: false),
-    );
-  }
-
-  Future<void> updateAccommodationType(
-    BuildContext context,
-    List<String> accommodationType,
-  ) async {
-    preferences?.accommodationTypes = Set<String>.from(accommodationType);
-    await UserDatabaseHandler.instance.insertOrUpdateUser(
-      this,
-      Provider.of<AuthModel>(context, listen: false),
+      gender: map['gender'],
+      age: map['age'],
+      height: map['height'],
+      weight: map['weight'],
     );
   }
 }
